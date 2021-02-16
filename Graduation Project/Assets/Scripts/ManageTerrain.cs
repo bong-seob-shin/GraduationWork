8db7637ddd21ffcceec5690ebf71fd0778850e7c
SBS <llkey12@naver.com>
2021-02-16 20:35:00 +0900

    맵툴 수정 보완

--- Graduation Project/Assets/Scripts/ManageTerrain.cs
+++ Graduation Project/Assets/Scripts/ManageTerrain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using System;
 
 public class ManageTerrain : EditorWindow
 {
@@ -23,36 +24,36 @@ public class ManageTerrain : EditorWindow
     }
 
 
-    void AddTerrain()
-    {
-        if (Selection.activeGameObject == null)
-        {
-            Debug.LogWarning("no terrain was selected");
-            return;
-        }
-
-        if (Selection.activeGameObject.GetComponent<Terrain>() == null)
-        {
-            Debug.LogWarning("this object is not Terrain");
-            return;
-        }
-        
-        bool isTerrainInList = false;
-        foreach (var gameObj in terrainObjs)
-        {
-            if (Selection.activeGameObject.name == gameObj.name)
-            {
-                Debug.LogWarning("already this terrain exit");
-                isTerrainInList = true;
-            }
-        }
-
-        if (!isTerrainInList)
-        {
-            terrainObjs.Add(Selection.activeGameObject);
-        }
-        
-    }
+    // void AddTerrain()
+    // {
+    //     if (Selection.activeGameObject == null)
+    //     {
+    //         Debug.LogWarning("no terrain was selected");
+    //         return;
+    //     }
+    //
+    //     if (Selection.activeGameObject.GetComponent<Terrain>() == null)
+    //     {
+    //         Debug.LogWarning("this object is not Terrain");
+    //         return;
+    //     }
+    //     
+    //     bool isTerrainInList = false;
+    //     foreach (var gameObj in terrainObjs)
+    //     {
+    //         if (Selection.activeGameObject.name == gameObj.name)
+    //         {
+    //             Debug.LogWarning("already this terrain exit");
+    //             isTerrainInList = true;
+    //         }
+    //     }
+    //
+    //     if (!isTerrainInList)
+    //     {
+    //         terrainObjs.Add(Selection.activeGameObject);
+    //     }
+    //     
+    // }
 
     
     void AddTerrains()
@@ -65,6 +66,19 @@ public class ManageTerrain : EditorWindow
             {
                 terrainObjs.Add(gObj);
             }
+            terrainObjs.Sort(delegate(GameObject p_gobj, GameObject n_gobj)
+            {
+                if (String.Compare(p_gobj.name, n_gobj.name) == 1)
+                {
+                    return 1;
+                }
+
+                if (String.Compare(p_gobj.name, n_gobj.name) == -1)
+                {
+                    return -1;
+                }
+                else return 0;
+            });
         }
         
     }
@@ -78,12 +92,12 @@ public class ManageTerrain : EditorWindow
     }
     void OnGUI()
     {
-        GUILayout.Space(10);
+        //GUILayout.Space(10);
 		
-        if(GUILayout.Button("Add Terrain"))
-        {			
-            AddTerrain();							
-        }
+        // if(GUILayout.Button("Add Terrain"))
+        // {			
+        //     AddTerrain();							
+        // }
         
         GUILayout.Space(10);
 		
@@ -96,6 +110,7 @@ public class ManageTerrain : EditorWindow
         if(GUILayout.Button("Init Terrain"))
         {			
             terrainObjs.Clear();				
+            terrainObjs = new List<GameObject>();
         }
         
         GUILayout.Space(10);
@@ -110,37 +125,214 @@ public class ManageTerrain : EditorWindow
         }
     
         GUILayout.Space(10);
-        if (terrainObjs.Count != 0)
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("0"))
         {
-            GUILayout.Box("now Terrain :" + terrainObjs[index].name,GUILayout.Width(200));
+            if (terrainObjs[0].activeSelf)
+            {
+                terrainObjs[0].SetActive(false);
+            }
+            else if (!terrainObjs[0].activeSelf)
+            {
+                terrainObjs[0].SetActive(true);
+            }
+
         }
+       
+        if (GUILayout.Button("1"))
+        {
+            if (terrainObjs[1].activeSelf)
+            {
+                terrainObjs[1].SetActive(false);
+            }
+            else if (!terrainObjs[1].activeSelf)
+            {
+                terrainObjs[1].SetActive(true);
+            }
+        }
+
+        if (GUILayout.Button("2"))
+        {
+            if (terrainObjs[2].activeSelf)
+            {
+                terrainObjs[2].SetActive(false);
+            }
+            else if (!terrainObjs[2].activeSelf)
+            {
+                terrainObjs[2].SetActive(true);
+            }
+        }
+
+        if (GUILayout.Button("3"))
+        {
+            if (terrainObjs[3].activeSelf)
+            {
+                terrainObjs[3].SetActive(false);
+            }
+            else if (!terrainObjs[3].activeSelf)
+            {
+                terrainObjs[3].SetActive(true);
+            }
+        }
+        GUILayout.EndHorizontal();
+        
+
+        GUILayout.Space(10);
+        
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("4"))
+        {
+            if (terrainObjs[4].activeSelf)
+            {
+                terrainObjs[4].SetActive(false);
+            }
+            else if (!terrainObjs[4].activeSelf)
+            {
+                terrainObjs[4].SetActive(true);
+            }
+        }
+
+        if (GUILayout.Button("5"))
+        {
+            if (terrainObjs[5].activeSelf)
+            {
+                terrainObjs[5].SetActive(false);
+            }
+            else if (!terrainObjs[5].activeSelf)
+            {
+                terrainObjs[5].SetActive(true);
+            }
+        }
+
+        if (GUILayout.Button("6"))
+        { 
+            if (terrainObjs[6].activeSelf)
+            {
+                terrainObjs[6].SetActive(false);
+            }
+            else if (!terrainObjs[6].activeSelf)
+            {
+                terrainObjs[6].SetActive(true);
+            }
+        }
+
+        if (GUILayout.Button("7"))
+        {
+            if (terrainObjs[7].activeSelf)
+            {
+                terrainObjs[7].SetActive(false);
+            }
+            else if (!terrainObjs[7].activeSelf)
+            {
+                terrainObjs[7].SetActive(true);
+            }
+        }
+        GUILayout.EndHorizontal();
         GUILayout.Space(10);
 
         GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Previos"))
+        if (GUILayout.Button("8"))
         {
-            if (index > 0)
+            if (terrainObjs[8].activeSelf)
             {
-                index--;
+                terrainObjs[8].SetActive(false);
             }
-            SelectTerrain(index);
-            Repaint();
+            else if (!terrainObjs[8].activeSelf)
+            {
+                terrainObjs[8].SetActive(true);
+            }
         }
-        
-        GUILayout.Space(100);
-        
-        if (GUILayout.Button("Next"))
+       
+        if (GUILayout.Button("9"))
         {
-            if (index < terrainObjs.Count-1)
+            if (terrainObjs[9].activeSelf)
             {
-                index++;
+                terrainObjs[9].SetActive(false);
             }
-            SelectTerrain(index);
-            Repaint();
+            else if (!terrainObjs[9].activeSelf)
+            {
+                terrainObjs[9].SetActive(true);
+            }
         }
 
+        if (GUILayout.Button("10"))
+        {
+            if (terrainObjs[10].activeSelf)
+            {
+                terrainObjs[10].SetActive(false);
+            }
+            else if (!terrainObjs[10].activeSelf)
+            {
+                terrainObjs[10].SetActive(true);
+            }
+        }
 
+        if (GUILayout.Button("11"))
+        {
+            if (terrainObjs[11].activeSelf)
+            {
+                terrainObjs[11].SetActive(false);
+            }
+            else if (!terrainObjs[11].activeSelf)
+            {
+                terrainObjs[11].SetActive(true);
+            }
+        }
+        GUILayout.EndHorizontal();
+        
 
+        GUILayout.Space(10);
+        
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("12"))
+        {
+            if (terrainObjs[12].activeSelf)
+            {
+                terrainObjs[12].SetActive(false);
+            }
+            else if (!terrainObjs[12].activeSelf)
+            {
+                terrainObjs[12].SetActive(true);
+            }
+        }
+
+        if (GUILayout.Button("13"))
+        {
+            if (terrainObjs[13].activeSelf)
+            {
+                terrainObjs[13].SetActive(false);
+            }
+            else if (!terrainObjs[13].activeSelf)
+            {
+                terrainObjs[13].SetActive(true);
+            }
+        }
+
+        if (GUILayout.Button("14"))
+        {
+            if (terrainObjs[14].activeSelf)
+            {
+                terrainObjs[14].SetActive(false);
+            }
+            else if (!terrainObjs[14].activeSelf)
+            {
+                terrainObjs[14].SetActive(true);
+            }
+        }
+
+        if (GUILayout.Button("15"))
+        {
+            if (terrainObjs[15].activeSelf)
+            {
+                terrainObjs[15].SetActive(false);
+            }
+            else if (!terrainObjs[15].activeSelf)
+            {
+                terrainObjs[15].SetActive(true);
+            }
+        }
+        GUILayout.EndHorizontal();
     }
 }
 
