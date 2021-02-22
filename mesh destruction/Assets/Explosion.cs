cc9eedb93008c1d9cb50007f41c28c58afebcf66
BDY <honey1586@naver.com>
2021-02-22 18:03:45 +0900

    mesh destruction

---
+++ mesh destruction/Assets/Explosion.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float cubeSize = 0.2f;
+    public int cubesInRow = 5;
+
+    public float explosionRadius;
+    public float explosionForce;
+    public float explosionUpward;
+
+    private float cubesPivotDistance;
+    private Vector3 cubesPivot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubesPivotDistance = cubeSize * cubesInRow / 2;
+        cubesPivot = new Vector3(cubesPivotDistance, cubesPivotDistance, cubesPivotDistance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Floor")
+        {
+            explode();
+        } 
+        
+    }
+
+    private void explode()
+    {
+        gameObject.SetActive(false);
+
+        for (int x = 0; x < cubesInRow; x++)
+        {
+            for (int y = 0; y < cubesInRow; y++)
+            {
+                for (int z = 0; z < cubesInRow; z++)
+                {
+                    createPiece(x, y, z);
+                }
+            }
+        }
+
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce,transform.position,explosionRadius,explosionUpward);
+            }
+        }
+    }
+
+    void createPiece(int x ,int y , int z)
+    {
+        GameObject piece;
+        piece = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        piece.transform.position = transform.position + new Vector3(cubeSize * x, cubeSize * y, cubeSize * z) - cubesPivot;
+        piece.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
+
+        piece.AddComponent<Rigidbody>();
+        piece.GetComponent<Rigidbody>().mass = 0.2f;
+    }
+}
