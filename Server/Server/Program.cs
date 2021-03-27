3bef1ff1f61de482f1571c739c4624bfac392abd
Yong0507 <62415286+Yong0507@users.noreply.github.com>
2021-03-27 15:57:16 +0900

    Revert "Timer 기능"
    This reverts commit 728c7cf407642666ec9aa83865fbef5fd01e6203.

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -13,12 +13,6 @@ namespace Server
     {
         static Listener _listener = new Listener();
         public static GameRoom Room = new GameRoom();
-       
-        static void FlushRoom()
-        {
-            Room.Push(() => Room.Flush());
-            JobTimer.Instance.Push(FlushRoom, 250);
-        }
 
         static void Main(string[] args)
         {
@@ -30,14 +24,11 @@ namespace Server
             _listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); }); // 리스너는 무엇을 만들어 줄지만 결정
             Console.WriteLine("Listening...");
 
-            //FlushRoom();
-            JobTimer.Instance.Push(FlushRoom);
-
             while (true)
             {
                 // 이 부분은 주 스레드, 비동기 AcceptCompleted는 작업자 스레드 
                 // 따라서 2개의 부분이 서로 쓰레드가 건드리기 때문에 data race 조심해야함
-                JobTimer.Instance.Flush();
+                ; 
             }
         }
     }
