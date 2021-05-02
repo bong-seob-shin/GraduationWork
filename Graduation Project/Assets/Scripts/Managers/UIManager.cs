6a801766cf73602b14d5a03be84ef9cc84c40422
SBS <llkey12@naver.com>
2021-05-03 00:33:58 +0900

    자동차 탑승 모델 추가, UI 제작
    추가적으로 기획한 컨셉 그림 안넣어 놔서 넣어놓았음

---
+++ Graduation Project/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI hpText;
+    public TextMeshProUGUI bulletText;
+
+    public Image menuImg;
+    public GameObject gameUI;
+    private bool isMenuOn = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isMenuOn = !isMenuOn;
+            menuImg.gameObject.SetActive(isMenuOn);
+            gameUI.SetActive(!isMenuOn);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ResumeGame()
+    {
+        menuImg.gameObject.SetActive(false);
+        gameUI.SetActive(true);
+        isMenuOn = false;
+    }
+}
