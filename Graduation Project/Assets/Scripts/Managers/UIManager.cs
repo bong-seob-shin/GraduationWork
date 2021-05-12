6b9b240b252ce93949786d03769cd93c4c627e10
SBS <llkey12@naver.com>
2021-05-13 00:17:05 +0900

    마우스 잠금 구현
    마우스 잠금 기능을 구현하였습니다.

--- Graduation Project/Assets/Scripts/Managers/UIManager.cs
+++ Graduation Project/Assets/Scripts/Managers/UIManager.cs
@@ -14,13 +14,14 @@ public class UIManager : MonoBehaviour
     public Image menuImg;
     public Image centerPoint;
     public GameObject gameUI;
-    private bool isMenuOn = false;
+    [SerializeField]private bool isMenuOn = false;
 
     public SliceController mySController;
     
     private void Awake()
     {
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -32,7 +33,18 @@ public class UIManager : MonoBehaviour
             menuImg.gameObject.SetActive(isMenuOn);
             gameUI.SetActive(!isMenuOn);
             centerPoint.gameObject.SetActive(!isMenuOn);
-            Cursor.visible = isMenuOn;
+            
+            if (isMenuOn)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+
+            }
             mySController.enabled = !isMenuOn;
         }
     }
@@ -49,6 +61,8 @@ public class UIManager : MonoBehaviour
         menuImg.gameObject.SetActive(false);
         centerPoint.gameObject.SetActive(true);
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
         gameUI.SetActive(true);
         isMenuOn = false;
         mySController.enabled = true;
