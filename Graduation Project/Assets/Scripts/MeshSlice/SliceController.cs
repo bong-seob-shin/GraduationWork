e632a5bfbd6faedfa8893e0f7b9d1956cdfc3220
BDY <honey1586@naver.com>
2021-05-16 02:20:22 +0900

    Modify Mesh slice script
    기존 Mesh Slice의 범위를 총으로 맞춘 메쉬만 자르도록 변경하였고, SliceableObject 스크립트가 붙어있는 모든 메시를 자르던 것을 총알이 맞은 메시만 자르도록 수정하였음.

--- Graduation Project/Assets/Scripts/MeshSlice/SliceController.cs
+++ Graduation Project/Assets/Scripts/MeshSlice/SliceController.cs
@@ -6,8 +6,12 @@ public class SliceController : MonoBehaviour {
 	bool isSlicing = false;
 	GameObject startUI, endUI, lineUI;
 	RectTransform lineRectTransform;
+	public Camera camera;
 	float lineWidth;
 	
+	private SliceableObject sliceableTarget;
+
+	private GameObject target;
 	// Use this for initialization
 	void Start () {
 		// UV Mapping Camera
@@ -33,58 +37,81 @@ public class SliceController : MonoBehaviour {
 	void Update () {
 		Vector3 mousePos;
 		// 마우스가 눌렸을 때 그 좌표를 시작 지점으로 한다.
-		if (Input.GetMouseButtonDown (0)) {
-			isSlicing = true;
-			mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f);
-			sliceStartPos = Camera.main.ScreenToWorldPoint(mousePos);
-			startUI.SetActive(true);
-			startUI.transform.position = Input.mousePosition;
-		} else if(Input.GetMouseButtonUp (0) && isSlicing) {
-			isSlicing = false;
-			mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f); // "z" value defines distance from camera
-			sliceEndPos = Camera.main.ScreenToWorldPoint(mousePos);
-			if(sliceStartPos != sliceEndPos) {
-				mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
-				Vector3 point3 = Camera.main.ScreenToWorldPoint(mousePos);
-				MeshSlicer.CustomPlane plane = new MeshSlicer.CustomPlane(sliceStartPos, sliceEndPos, point3);
-				Slice(plane);
+		RaycastHit hit;
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+		int layerMask = (-1) - (1 << LayerMask.NameToLayer("PlayerCamera"));
+		if(Physics.Raycast(ray, out hit , 100.0f,layerMask))
+		{
+			if (hit.transform.CompareTag("Sliceable"))
+			{
+				
+				//sliceableTarget = (SliceableObject)FindObjectOfType(typeof(SliceableObject));
+				target = hit.transform.gameObject;
+				if (Input.GetMouseButtonDown (0)) {
+					isSlicing = true;
+					mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f);
+					sliceStartPos = Camera.main.ScreenToWorldPoint(mousePos);
+					startUI.SetActive(true);
+					startUI.transform.position = Input.mousePosition;
+				} else if(Input.GetMouseButtonUp (0) && isSlicing) {
+					isSlicing = false;
+					mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f); // "z" value defines distance from camera
+					sliceEndPos = Camera.main.ScreenToWorldPoint(mousePos);
+					if(sliceStartPos != sliceEndPos) {
+						mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+						Vector3 point3 = Camera.main.ScreenToWorldPoint(mousePos);
+						MeshSlicer.CustomPlane plane = new MeshSlicer.CustomPlane(sliceStartPos, sliceEndPos, point3);
+						Slice(plane);
+					}
+				}
+				if(isSlicing) {
+					endUI.SetActive(true);
+					lineUI.SetActive(true);
+					endUI.transform.position = Input.mousePosition;
+					Vector2 linePos = (endUI.transform.position + startUI.transform.position) / 2f;
+					float lineHeight = (endUI.transform.position - startUI.transform.position).magnitude;
+					Vector3 lineDir = (endUI.transform.position - startUI.transform.position).normalized;
+					lineUI.transform.position = linePos;
+					lineRectTransform.sizeDelta = new Vector2(lineWidth, lineHeight);
+					lineUI.transform.rotation = Quaternion.FromToRotation(Vector3.up, lineDir);
+				} else {
+					startUI.SetActive(false);
+					endUI.SetActive(false);
+					lineUI.SetActive(false);
+				}
 			}
-		}
-		if(isSlicing) {
-			endUI.SetActive(true);
-			lineUI.SetActive(true);
-			endUI.transform.position = Input.mousePosition;
-			Vector2 linePos = (endUI.transform.position + startUI.transform.position) / 2f;
-			float lineHeight = (endUI.transform.position - startUI.transform.position).magnitude;
-			Vector3 lineDir = (endUI.transform.position - startUI.transform.position).normalized;
-			lineUI.transform.position = linePos;
-			lineRectTransform.sizeDelta = new Vector2(lineWidth, lineHeight);
-			lineUI.transform.rotation = Quaternion.FromToRotation(Vector3.up, lineDir);
-		} else {
-			startUI.SetActive(false);
-			endUI.SetActive(false);
-			lineUI.SetActive(false);
 		}
 	}
 	
 	// Detect & slice "sliceable" GameObjects whose bounding box intersects slicing plane
 	private void Slice(MeshSlicer.CustomPlane plane) {
-		SliceableObject[] sliceableTargets = (SliceableObject[])FindObjectsOfType( typeof(SliceableObject) );
+		// SliceableObject[] sliceableTargets = (SliceableObject[])FindObjectsOfType( typeof(SliceableObject) );
+		// bool isSliced = false;
+		// foreach(SliceableObject sliceableTarget in sliceableTargets) {
+		// 	GameObject target = sliceableTarget.gameObject;
+		// 	if(plane.HitTest(target)) {
+		// 		if(target.GetComponent<SliceableObject>().isConvex) {
+		// 			MeshSlicer.SliceMesh(target, plane, true);
+		// 			//audioSource.PlayOneShot(sliceableTarget.sliceSound, 1.0f);
+		// 			isSliced = true;
+		// 		} else {
+		// 			MeshSlicer.SliceMesh(target, plane, false);
+		// 			//audioSource.PlayOneShot(sliceableTarget.sliceSound, 1.0f);
+		// 			isSliced = true;
+		// 		}
+		// 	}
+		// }
+		
 		bool isSliced = false;
-		foreach(SliceableObject sliceableTarget in sliceableTargets) {
-			GameObject target = sliceableTarget.gameObject;
-			if(plane.HitTest(target)) {
-				if(target.GetComponent<SliceableObject>().isConvex) {
-					MeshSlicer.SliceMesh(target, plane, true);
-					//audioSource.PlayOneShot(sliceableTarget.sliceSound, 1.0f);
-					isSliced = true;
-				} else {
-					MeshSlicer.SliceMesh(target, plane, false);
-					//audioSource.PlayOneShot(sliceableTarget.sliceSound, 1.0f);
-					isSliced = true;
-				}
+		if(plane.HitTest(target)) {
+			if(target.GetComponent<SliceableObject>().isConvex) {
+				MeshSlicer.SliceMesh(target, plane, true);
+				isSliced = true;
+			} else {
+				MeshSlicer.SliceMesh(target, plane, false);
+				isSliced = true;
 			}
-		}
+		}	
 		
 	}
 }
