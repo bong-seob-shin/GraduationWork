4b3f38048798748fbabc7e205d68b70bba6b3713
BDY <honey1586@naver.com>
2021-05-27 23:58:43 +0900

    add 보스 프로토타입 모델 생성 및 스크립트 생성
    보스 모델링 완성 전 컨셉에 맞추어 cube로 보스 몬스터 가상 모델 배치 후 스크립트 생성

---
+++ Graduation Project/Assets/Scripts/Monster/BossMonster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMonster : MonoBehaviour
+{
+    public GameObject first_face;
+    public GameObject second_face;
+    public GameObject third_face;
+    public GameObject fourth_face;
+
+    public GameObject second_rightarm;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
