e2c9ca32b19c5607ab73eb4d4761d82cf2cae9df
SBS <llkey12@naver.com>
2021-05-29 22:02:50 +0900

    laser의 길이를 동적으로 변하게 수정
    laser의 boxcollider와 길이를 양쪽 동굴의 지점에 맞게 레이캐스트를 이용하여 조절하는 기능을 구현하였습니다.

--- Graduation Project/Assets/Scripts/Laser.cs
+++ Graduation Project/Assets/Scripts/Laser.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+    private LineRenderer _lineRenderer;
+
+    private BoxCollider _boxCollider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _lineRenderer = GetComponent<LineRenderer>();
+        _boxCollider = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        Physics.Raycast(transform.position, transform.right, out hit);
+        RaycastHit rightHitInfo;
+        RaycastHit leftHitInfo;
+
+        float rightDist =0;
+        float leftDist =0;
+
+        if (Physics.Raycast(transform.position, transform.right, out rightHitInfo, 50.0f))
+        {
+            if (rightHitInfo.transform.CompareTag("Cave"))
+            {
+                rightDist =  (rightHitInfo.point-transform.position).magnitude;
+                _lineRenderer.SetPosition(0, new Vector3( rightDist, 0 , 0));
+            }
+        }
+        
+        if (Physics.Raycast(transform.position, -transform.right, out leftHitInfo, 50.0f))
+        {
+            if (leftHitInfo.transform.CompareTag("Cave"))
+            {
+                leftDist = (leftHitInfo.point -transform.position).magnitude ;
+                _lineRenderer.SetPosition(1, new Vector3(-leftDist,0,0) );
+            }
+        }
+        
+        _boxCollider.size = new Vector3(leftDist+rightDist,1,1);
     }
 }
