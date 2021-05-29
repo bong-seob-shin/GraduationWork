a876c378394e54455b7bd4d7e48e0176f185a49e
BDY <honey1586@naver.com>
2021-05-29 18:57:09 +0900

    add bossmonster thirdPattern
    보스몬스터 두번째 패턴 수정 및 세번째 패턴 추가

--- Graduation Project/Assets/Scripts/Monster/BossMonsterBullet.cs
+++ Graduation Project/Assets/Scripts/Monster/BossMonsterBullet.cs
@@ -1,10 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossMonsterBullet : MonoBehaviour
+public class BossMonsterBullet : ObjManager
 {
     public int damage = 30;
+    public float speed = 10.0f;
+
+    [SerializeField] private Vector3 target;
+    private Vector3 dir;
+    
+    private void Update()
+    {
+        transform.position += dir * Time.deltaTime * speed;
+        
+    }
+
+    public void SetTarget(Vector3 t)
+    {
+        target = t;
+        dir = (target - transform.position).normalized;
+        dir += new Vector3(0.0f, 0.02f, 0.0f);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -14,6 +33,11 @@ public class BossMonsterBullet : MonoBehaviour
         }
 
         if (other.CompareTag("Terrain"))
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (other.CompareTag("Cave"))
         {
             Destroy(this.gameObject);
         }
