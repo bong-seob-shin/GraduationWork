b4223402a8f6fb78501edca9a6a01a8727dedcdb
BDY <honey1586@naver.com>
2021-06-02 22:40:40 +0900

    modify fourthpattern
    코어 찌그러짐 수정.

--- Graduation Project/Assets/Scripts/Monster/Core.cs
+++ Graduation Project/Assets/Scripts/Monster/Core.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Core : ObjManager
 {
+    private Transform _parent;
     // Start is called before the first frame update
     void Start()
     {
+        _parent = transform.parent;
+        transform.parent = null;
+        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        transform.parent = _parent.parent;
         MaxHP = 80;
         HP = MaxHP;
     }
@@ -16,7 +21,7 @@ public class Core : ObjManager
     {
         if (HP <= 0)
         {
-            Destroy(this.transform.parent.gameObject);
+            Destroy(_parent.gameObject);
             Destroy(this.gameObject);
         }
     }
