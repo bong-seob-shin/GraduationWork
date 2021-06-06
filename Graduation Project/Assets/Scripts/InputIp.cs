d157551a3fc1597dde48ae66996c0707ad8c34f9
SBS <llkey12@naver.com>
2021-06-06 22:53:52 +0900

    ipInput Scene 제작
    ip를 입력받기 위한 IpInputScene을 제작하여 입력받은 ip를 다음 씬에 전달해주는 기능을 구현하였습니다.

---
+++ Graduation Project/Assets/Scripts/InputIp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InputIp : MonoBehaviour
+{
+    public Button connectButton;
+
+    public InputField ipInput;
+
+    private String ip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onConnectBtnClick()
+    {
+        ip = ipInput.text;
+
+        SceneManager.LoadScene(1);
+        Debug.Log(ip);
+    }
+}
+
