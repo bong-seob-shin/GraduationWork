406b004372657a2f62f6f0b667b9dbd1e4dc75d1
SBS <llkey12@naver.com>
2021-07-15 02:58:36 +0900

    던전2 2층 바닥 문 트리거 제작
    PuzzleDoorTrigger스크립트를 작성하여
    바닥에 있는 버튼을 모두 누르면 문이 열리는 방식을 구현하였습니다.

---
+++ Graduation Project/Assets/Scripts/InteractiveObj/PuzzleDoorTrigger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleDoorTrigger : MonoBehaviour
+{
+    public enum eTrigger
+    {
+        Lamp,None
+    }
+
+    public eTrigger triggerType;
+
+    public GameObject[] triggers;
+
+    private bool isPlayed = false;
+    private bool isPuzzleCorrected = false;
+    
+    public Animation doorAnim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPlayed)
+        {
+            if (triggerType == eTrigger.Lamp)
+            {
+                foreach (var go in triggers)
+                {
+                    if (go.GetComponent<OnOffLamp>().isOnOff)
+                    {
+                        isPuzzleCorrected = true;
+                    }
+                    else
+                    {
+                        isPuzzleCorrected = false;
+                        break;
+                    }
+                    
+                }
+            }
+
+
+            if (isPuzzleCorrected)
+            {
+                doorAnim.Play();
+                isPlayed = true;
+            }
+        }
+    }
+}
