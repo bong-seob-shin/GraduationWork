2a8d2b4a9f18bcb6815b7229ec60472689762896
BDY <honey1586@naver.com>
2021-07-15 22:10:20 +0900

    Modify Door.cs , Gun.cs
    코드 원래대로 복구

--- Graduation Project/Assets/Scripts/Weapon/Gun.cs
+++ Graduation Project/Assets/Scripts/Weapon/Gun.cs
@@ -44,14 +44,11 @@ public class Gun : MonoBehaviour
     private float reloadTime = 0f;
     // Start is called before the first frame update
     
-
     
-    /// <summary>
-    /// mesh slice
     public Material insideMaterial;
     public float loadTime;
     public int generations;
-    /// </summary>
+    
     void Start()
     {
         gunAnim = GetComponent<Animation>();
@@ -153,12 +150,13 @@ public class Gun : MonoBehaviour
                     planeCore.hit(damage,penetration);
                 }
                 
-                if (hit.transform.gameObject.tag == "Sliceable")
+                if (door != null)
                 {
                     door.hit(damage,0.0f);
                     if (door.HP <= 0.0f)
                     {
-                        door.GetComponent<Door>().slicePos = hit.point;
+                        insideMaterial = hit.transform.GetComponent<MeshRenderer>().materials.ElementAt(0);
+                        StartCoroutine(ShatterObject(hit.transform.gameObject, hit.point, generations));
                     }
                 }
                 
@@ -176,7 +174,34 @@ public class Gun : MonoBehaviour
         
     }
 
+    private IEnumerator ShatterObject(GameObject obj,Vector3 hitpoint,int gen)
+    {
+        yield return new WaitForSeconds(Random.Range(loadTime, loadTime * 2));
+        
+        GameObject[] pieces = MeshManipulation.MeshCut.Cut(obj, /*obj.GetComponent<Collider>().bounds.center*/ hitpoint, GetAngle(obj,gen), insideMaterial);
 
+        foreach (GameObject piece in pieces)
+        {
+            piece.AddComponent<Rigidbody>().ResetCenterOfMass();
+            piece.AddComponent<MeshCollider>().convex = true;
+
+            piece.GetComponent<Rigidbody>().AddForce(transform.forward * 2.0f , ForceMode.Impulse);
+            piece.transform.tag = "Sliceable";
+            if (gen > 0)
+                StartCoroutine(ShatterObject(piece, hitpoint, gen - 1));
+
+            piece.AddComponent<MeshDestroy>();
+        }
+
+        Destroy(obj);
+
+    }
+    private Vector3 GetAngle(GameObject obj , int gen)
+    {
+        Quaternion q = Quaternion.Euler(Random.Range(-40, 40), Random.Range(-40, 40), Random.Range(-40, 40));
+        Vector3[] faces = {obj.transform.forward, obj.transform.right, obj.transform.up};
+        return q * faces[gen % 3];
+    }
 
     
     private void OnDrawGizmos()
